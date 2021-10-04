2a3e3625ad114e58865b15a36d90228c23e96a4f
ah420mad <72621510+ah420mad@users.noreply.github.com>
2021-10-04 14:47:35 +0200

    reorganisation

--- Lab1/Express/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const handles = require("./routes/handles");
-
-const app = express();
-
-app.use("/hello", handles);
-
-app.listen(8080);
--- Lab1/Node/index.js
+++
@@ -1,5 +0,0 @@
-const http = require("http");
-const handles = require("./handles");
-
-const server = http.createServer(handles.serverHandle);
-server.listen(8080);
