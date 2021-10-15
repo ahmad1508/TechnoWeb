4f0dd2e8d2d59c6dc7380a6622ba7133d0a3fe41
Sergei Kudinov <sergkudinov@gmail.com>
2021-10-15 14:18:44 +0200

    fix: messages list

--- test/messages.js
+++ test/messages.js
@@ -24,19 +24,27 @@ describe('messages', () => {
   it('list one message', async () => {
     // Create a channel
     const {body: channel} = await supertest(app)
-    .post('/channels')
-    .send({name: 'channel 1'})
+      .post('/channels')
+      .send({name: 'channel 1'})
+    // TODO create a user
+    // ...
+    const author = '12343543'
     // and a message inside it
+    const message = 'Hello ECE'
     await supertest(app)
-    .post(`/channels/${channel.id}/messages`)
-    .send({content: 'Hello ECE'})
+      .post(`/channels/${channel.id}/messages`)
+      .send({
+        content: message,
+        author: author
+      })
     // Get messages
     const {body: messages} = await supertest(app)
-    .get(`/channels/${channel.id}/messages`)
-    .expect(200)
+      .get(`/channels/${channel.id}/messages`)
+      .expect(200)
     messages.should.match([{
       creation: (it) => it.should.be.approximately(Date.now(), 1000),
-      content: 'Hello ECE'
+      content: message,
+      author: author
     }])
   })
   
