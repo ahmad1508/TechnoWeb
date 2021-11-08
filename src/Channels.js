63ac73054615f710b89097dac36208f204326ed1
AhmadSONJI <ahmad.sonji01@gmail.com>
2021-11-08 14:14:16 +0100

    lab4

--- src/Channels.js
+++ src/Channels.js
@@ -1,19 +1,17 @@
 
 /** @jsxImportSource @emotion/react */
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 import axios from 'axios';
-import { Button, Drawer } from '@mui/material';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import { Link } from '@mui/material';
 // Layout
+import { Link } from '@mui/material';
+
 const styles = {
   root: {
     minWidth: '200px',
   },
   channel: {
     padding: '.2rem .5rem',
-    whiteSpace: 'nowrap',
+    whiteSpace: 'nowrap', 
   }
 }
 
@@ -21,30 +19,24 @@ export default function Channels({
   onChannel
 }) {
   const [channels, setChannels] = useState([])
-  const [state, setState] = useState(false);
-  const toggleDrawer = (open) => (e) => {
-    setState(open);
-  }
-
-  useEffect(() => {
+  useEffect( () => {
     const fetch = async () => {
-      const { data: channels } = await axios.get('http://localhost:3001/channels')
+      const {data: channels} = await axios.get('http://localhost:3001/channels')
       setChannels(channels)
     }
     fetch()
   }, [])
-
   return (
     <ul style={styles.root}>
-      {channels.map((channel, i) => (
+      { channels.map( (channel, i) => (
         <li key={i} css={styles.channel}>
           <Link
             href="#"
-            onClick={(e) => {
+            onClick={ (e) => {
               e.preventDefault()
               onChannel(channel)
             }}
-          >
+            >
             {channel.name}
           </Link>
         </li>
