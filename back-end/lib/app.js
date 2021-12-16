dbdb05f801b2b377e4f5f5fd9cc1aa550d6ec4c1
MrStaf <ben82160@gmail.com>
2021-12-16 13:03:52 +0100

    dropdown working

--- back-end/lib/app.js
+++ back-end/lib/app.js
@@ -28,7 +28,6 @@ app.get("/channels", authenticate, async (req, res) => {
         break;
       } else if (channel.email === req.user.email) {
         channels.push(channel)
-
         break;
       } else {
       }
